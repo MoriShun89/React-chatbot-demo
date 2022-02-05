e3ae8cfa36f243b65d2c6da90b93b522a92b21d7
MoriShun89 <s.moririn0724@gmail.com>
2022-02-05 17:02:58 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import defaultDataset from './dataset'
+import './assets/styles/style.css'
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: [],
+      chats: [],
+      currentId: "init",
+      dataset: defaultDataset,
+      open: false
+    }
+  }
+
+  render() {
+    return (
+      <section class="c-section">
+        <div class="c-box"></div>
+      </section>
+    );
+  }
+}
+
