b89047ff9984ac4a376e56c5af980e35194986d7
MoriShun89 <s.moririn0724@gmail.com>
2022-02-09 16:35:40 +0900

    チャット機能作成

--- src/components/AnswersList.jsx
+++ src/components/AnswersList.jsx
@@ -6,13 +6,14 @@ const AnswersList = (props) => {
   return(
     <div className="c-grid__answer">
       {props.answers.map((value, index) => {
-        return 
+        return (
           <Answer 
             content={value.content} 
             nextId={value.nextId}
             key={index.toString()} 
             select={props.select}
           />
+        )
       })}
     </div>
   )
