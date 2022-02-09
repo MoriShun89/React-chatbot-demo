905ae72b3d3e383029b56d99e094d8d53be429fc
MoriShun89 <s.moririn0724@gmail.com>
2022-02-09 16:00:20 +0900

    【WIP】チャット機能作成

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -13,7 +13,11 @@ const Answer = (props) => {
   // const classes = useStyles();
 
   return(
-    <Button variant="contained" color="primary">
+    <Button 
+      variant="contained" 
+      color="primary" 
+      onClick={() => props.select(props.content, props.nextId)}
+    >
       {props.content}
     </Button>
   )
