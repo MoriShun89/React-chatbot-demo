867f462a8395da834642fc8dbfb30cff4d346e89
MoriShun89 <s.moririn0724@gmail.com>
2022-03-06 19:06:21 +0900

    問い合わせモーダル作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import defaultDataset from './dataset'
 import './assets/styles/style.css'
 import {AnswersList, Chats} from './components/index'
+import FormDialog from './components/Forms/FormDialog';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,6 +15,8 @@ export default class App extends React.Component {
       open: false
     }
     this.selectAnswer = this.selectAnswer.bind(this)
+    this.handleClickOpen = this.handleClickOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
   displayNextQuestion = (nextQuestionId) => {
@@ -35,12 +38,18 @@ export default class App extends React.Component {
       case (nextQuestionId === "init") :
         setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
         break;
+
+      case (nextQuestionId === "contact") :
+        this.handleClickOpen();
+        break;
+
       case (/^https:*/.test(nextQuestionId)) :
         const a = document.createElement("a");
         a.href = nextQuestionId;
         a.target = "_blank"
         a.click();
         break;
+        
       default :
         const chats = this.state.chats;
         chats.push({
@@ -56,6 +65,14 @@ export default class App extends React.Component {
         break;
     }
   }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
 
   componentDidMount() {
     const initAnswer = "";
@@ -77,6 +94,10 @@ export default class App extends React.Component {
           <AnswersList 
             answers={this.state.answers} 
             select={this.selectAnswer}
+          />
+          <FormDialog 
+            open={this.state.open} 
+            handleClose={this.handleClose}
           />
         </div>
       </section>
