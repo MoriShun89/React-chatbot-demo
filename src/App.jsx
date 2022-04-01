1ff1cd8c648e097f9f7a78797669649a1d0c65fd
MoriShun89 <s.moririn0724@gmail.com>
2022-04-01 18:10:12 +0900

    App.jsxをクラスコンポーネントから関数コンポーネントに書き換え

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './assets/styles/style.css'
 import {AnswersList, Chats} from './components/index'
 import FormDialog from './components/Forms/FormDialog';
@@ -6,43 +6,27 @@ import { db } from './firebase/index'
 // 【Firebase ver.9の書き方】
 import { collection, getDocs } from 'firebase/firestore'
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      answers: [],
-      chats: [],
-      currentId: "init",
-      dataset: {},
-      open: false
-    }
-    this.selectAnswer = this.selectAnswer.bind(this)
-    this.handleClickOpen = this.handleClickOpen.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-  }
+const App = () => {
+  const [answers, setAnswers] = useState([]);
+  const [chats, setChats] = useState([]);
+  const [currentId, setCurrentID] = useState("init");
+  const [dataset, setDataset] = useState({});
+  const [open, setOpen] = useState(false);
 
-  displayNextQuestion = (nextQuestionId) => {
-    const chats = this.state.chats
-    chats.push({
-      text: this.state.dataset[nextQuestionId].question,
+  const displayNextQuestion = (nextQuestionId, nextDataset) => {
+    addChats({
+      text: nextDataset.question,
       type: 'question'
     })
 
-    this.setState({
-      answers: this.state.dataset[nextQuestionId].answers,
-      chats: chats,
-      currentId: nextQuestionId
-    })
+    setAnswers(nextDataset.answers)
+    setCurrentID(nextQuestionId)
   }
 
-  selectAnswer = (selectedAnswer, nextQuestionId) => {
+  const selectAnswer = (selectedAnswer, nextQuestionId) => {
     switch (true) {
-      case (nextQuestionId === "init") :
-        setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
-        break;
-
       case (nextQuestionId === "contact") :
-        this.handleClickOpen();
+        handleClickOpen();
         break;
 
       case (/^https:*/.test(nextQuestionId)) :
@@ -53,36 +37,33 @@ export default class App extends React.Component {
         break;
         
       default :
-        const chats = this.state.chats;
-        chats.push({
+        addChats({
           text: selectedAnswer,
           type: 'answer'
         })
 
-        this.setState({
-          chats: chats
-        })
-
-        setTimeout(() => this.displayNextQuestion(nextQuestionId), 1000);
+        setTimeout(() => displayNextQuestion(nextQuestionId, dataset[nextQuestionId]), 1000);
         break;
     }
   }
 
-  handleClickOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleClose = () => {
-    this.setState({ open: false });
-  };
-
-  initDataset = (dataset) => {
-    this.setState({dataset: dataset})
+  const addChats = (chat) => {
+    setChats(prevChats => {
+      return [...prevChats, chat]
+    })
   }
 
-  componentDidMount() {
+  const handleClickOpen = () => {
+    setOpen(true)
+  };
+
+  const handleClose = useCallback(() => {
+    setOpen(false)
+  }, [setOpen]);
+
+  useEffect(() => {
     (async() => {
-      const dataset = this.state.dataset
+      const initDataset = {};
 
       // 【Firebase ver.8の書き方（講座の内容）】
       // await db.collection('questions').get().then((snapshots) => {
@@ -99,39 +80,42 @@ export default class App extends React.Component {
         snapshots.forEach((doc) => {
           const id = doc.id
           const data = doc.data()
-          dataset[id] = data
+          initDataset[id] = data
         })
       })
 
-      this.initDataset(dataset)
-      const initAnswer = "";
-      this.selectAnswer(initAnswer, this.state.currentId)
+      setDataset(initDataset)
+      displayNextQuestion(currentId, initDataset[currentId])
     })()
-  }
+  // useEffect has a missing dependencyのwarningを解消する
+  // 参考：https://zenn.dev/mackay/articles/1e8fcce329336d
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-  componentDidUpdate() {
+  // 毎回のレンダリング時に実行
+  useEffect(() => {
     const scrollArea = document.getElementById("scroll-area")
     if(scrollArea) {
       scrollArea.scrollTop = scrollArea.scrollHeight
     }
-  }
+  })
 
-  render() {
-    return (
-      <section class="c-section">
-        <div class="c-box">
-          <Chats chats={this.state.chats} />
-          <AnswersList 
-            answers={this.state.answers} 
-            select={this.selectAnswer}
-          />
-          <FormDialog 
-            open={this.state.open} 
-            handleClose={this.handleClose}
-          />
-        </div>
-      </section>
-    );
-  }
+  return (
+    <section class="c-section">
+      <div class="c-box">
+        <Chats chats={chats} />
+        <AnswersList 
+          answers={answers} 
+          select={selectAnswer}
+        />
+        <FormDialog 
+          open={open} 
+          handleClose={handleClose}
+        />
+      </div>
+    </section>
+  );
 }
+
+export default App
 
